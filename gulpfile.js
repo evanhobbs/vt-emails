b6ff75b9bd3f8d1580ebfda6fd9dec15f15ffff6
evanhobbs <evanhobbs@gmail.com>
2015-09-13 13:27:00 -0700

    Basic email working - about to switch to zurb

--- gulpfile.js
+++ gulpfile.js
@@ -19,52 +19,70 @@ function startServer() {
 	 });
 }
 
-function getDirectories(srcpath) {
-  return fs.readdirSync(srcpath).filter(function(file) {
-    return fs.statSync(path.join(srcpath, file)).isDirectory();
-  });
+function getDirContents(srcpath, type) {
+	type = type || 'directories'
+	return fs.readdirSync(srcpath).filter(function(file) {
+		var result = fs.statSync(path.join(srcpath, file)).isDirectory();
+		if (type === 'directories') return result;
+		if (type === 'files') return !result;
+	});
 }
 
 function renderEmail(name, data) {
 	data = data || {};
 	// var templateDir = path.join(__dirname, 'templates', name);
 	// var distDir = path.join(__dirname, 'dist');
-	var email = new EmailTemplate(templatesDir + '/' + name)
+	var email = new EmailTemplate(templatesDir + '/' + name, {
+		juiceOptions: {
+			preserveMediaQueries: true,
+			removeStyleTags: true,
+			applyAttributesTableElements: true,
+			applyWidthAttributes: true,
+			preserveImportant: true
+		}
+	})
 
 	if (!fs.existsSync(distDir)) fs.mkdirSync(distDir);
 
 	email.render(data, function (err, results) {
 		if (err) return console.log(err);
 		if (results.html) fs.writeFile(distDir + '/' + name + '.html', results.html)
-		if (results.text) fs.writeFile(distDir + '/' + name + '.text', results.text)
+		if (results.text) fs.writeFile(distDir + '/' + name + '.txt', results.text)
 	})
 
 }
 
 function renderAll() {
-	var dirs = getDirectories(path.join(__dirname, 'templates'));
+	var dirs = getDirContents(path.join(__dirname, 'templates'));
 	dirs.forEach(function(dir) {
-		var data = require(path.join(templatesDir, dir, 'default.json'));
+		var data = require(path.join(templatesDir, dir, 'defaults.json'));
+		console.log(data)
 		renderEmail(dir, data);
 	})	
 }
 
-gulp.task('dev', function() {
+function createPreview() {
+	var templates = [];
 	var previewTemplate = require('./preview/index.ejs')
-	fs.writeFile('./preview/index.html', previewTemplate({
-		templates: [
-			{ filename: 'dist/test.html', subject: 'test' }
-		]
-	}));
-
-	startServer();
-	renderAll();
-	watch(['templates/**/*'], function() {
-		renderAll();
-		console.log('I should reload')
-		connect.reload();
+	//get the rendered templates
+	var files = getDirContents(path.join(__dirname, 'preview/dist'), 'files');
+	files.forEach(function(file) {
+		templates.push({ filename: 'dist/' + file, subject: file });
 	})
-	// renderEmail('test')
-	// server();
-	// browserSync();
+
+	fs.writeFile('./preview/index.html', previewTemplate({
+		templates: templates
+	}));	
+}
+
+gulp.task('dev', function() {
+	renderAll();
+	createPreview();
+	startServer();
+
+	watch(['include/**/*', 'templates/**/*', 'css/**/*'], function() {
+		renderAll();
+		createPreview();
+		gulp.src('preview/index.html').pipe(connect.reload())
+	})
 });
